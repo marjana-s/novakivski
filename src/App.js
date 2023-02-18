fcec327815e74917159ca0f437d2fcebc1270620
stasik132 <stas1705200563@gmail.com>
2023-02-18 03:14:39 +0200

    add a state Count

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-
+import React, { useEffect, useState } from 'react';
 
 
 function App() {
+
+  const [count,setCount] = useState(0);
+
   return (
     <div >
+
       hello world
       
     </div>
