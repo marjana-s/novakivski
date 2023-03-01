4f5c1ed69a64661defc859fa13b11db59958f4ad
stasik132 <stas1705200563@gmail.com>
2023-03-01 23:31:40 +0200

    add new page

--- src/components/MapRoad/MapRoad.jsx
+++ src/components/MapRoad/MapRoad.jsx
@@ -5,6 +5,9 @@ import Mother from '../../img/Mother.png'
 import Father from '../../img/Father.png'
 import Pumpkins from '../../img/Pumpkins.png'
 import Sticks from  '../../img/Sticks.png'
+import Yan from '../../img/Yan.png'
+import Leon from '../../img/Leon.png'
+import Fair from '../../img/Fair.png'
 function MapRoad() {
 
 
@@ -12,7 +15,7 @@ function MapRoad() {
   const [scrolled, setScrolled] = useState(false);
 
   const handleScroll = () => {
-      if (window.scrollY > 800 && window.scrollY <= 5100) {
+      if (window.scrollY > 800 && window.scrollY <= 5300) {
         setScrolled(true);
       } else if( window.scrollY > 6400){
         setScrolled(true);
@@ -60,7 +63,7 @@ function MapRoad() {
 
           <div className="page">
             <div className="left_side">
-              <p className="mid_margin_top">Oleksa Novakivsky's second school was nature itself. When he was 10 years old, he had a mystical incident on the way to the city. When he fell asleep during one of the stops, he had a dream. Ivan Holubovsky retells this story in his memoir, "A Spread of Mighty Wings". </p>
+              <p className="low_margin_top">Oleksa Novakivsky's second school was nature itself. When he was 10 years old, he had a mystical incident on the way to the city. When he fell asleep during one of the stops, he had a dream. Ivan Holubovsky retells this story in his memoir, "A Spread of Mighty Wings". </p>
               <div>
                 <p className="mid_margin_top">I bore my eyes into that face. I want to see the eyes of God! Eyes that I must paint, even if I have to die afterward. Dark stripes of eye features slowly begin to emerge from the magical shadows that play with the shimmering colors. As if they were closed in thought. I want to shout, I want to scream, to force them open, to open them. So that I could see them and remember those eyes that own the world and can shoot thunder. The eyes of God! </p>
                 <p className="low_margin_top">But I can't find the strength to cry out. Despair, impotent anguish grips my childish heart. I exert all my strength, rush forward, fly headlong into something, and suddenly ... wake up from my dream." Then I realized that I was alone in the forest. But the fright was followed by the thought that I had just received my father's permission to study painting, so this opportunity cannot be lost. So he went through the woods and met his brothers, who laughed that Oleksa had cut a path through the forest.</p>
@@ -68,7 +71,7 @@ function MapRoad() {
               </div>
             </div>
             <div className="right_side">
-              <p className="high_margin_top">"It was as if I was walking through an old forest among grasses and shady thickets on some important business. And as if an old bandura player approaches me. I had once seen him in an old blackened painting hanging in my teacher's apartment. The old man is blind with a strangely crooked smile and at the same time terrifyingly solemn. Tall as a giant, he bends down to work over me, takes my hand in his big, dry palm, and says in a voice that seems to have no sound." And the bandura player led little Oleksa through the alleys of giant roses to the giant tree. </p>
+              <p className="mid_margin_top">"It was as if I was walking through an old forest among grasses and shady thickets on some important business. And as if an old bandura player approaches me. I had once seen him in an old blackened painting hanging in my teacher's apartment. The old man is blind with a strangely crooked smile and at the same time terrifyingly solemn. Tall as a giant, he bends down to work over me, takes my hand in his big, dry palm, and says in a voice that seems to have no sound." And the bandura player led little Oleksa through the alleys of giant roses to the giant tree. </p>
               <p className="low_margin_top">"Take a good look, just take a good look," the old man whispers. "At first I don't see anything. There is only a haze around. Then I slowly begin to recognize a shocked face. I stare at it and the cold grabs me again. I begin to recognize a seemingly powerful head and in it a face made of wheat grains. Between them, there is hair from different grains. And these grains seem to be whirling, moving like the gentle waves of a flowing river. </p>
               <p className="mid_margin_top">Oleksa Novakivsky relied on the purpose he had found since childhood and honed throughout his life: to manifest the nature of beauty in the everyday. That is why he went to the final work through dozens of working drawings and paintings. And only at the end of his life was he so satisfied with the style he had found that he said, "Now I am finally the master of my brush."</p>
             </div>
@@ -76,7 +79,7 @@ function MapRoad() {
 
           <div className="page">
             <div className="left_side">
-              <p className="high_margin_top">Here is what Golubovsky writes about Oleksa's first training in A Spread of Mighty Wings. "He was a man of talent, but he had long been prim. He worked according to the patterns he had learned. Nevertheless, Oleksa Novakivsky learned a lot from him from a technical point of view. There he first began to master the technique of painting, and got acquainted with paints, their chemical composition, their proper use, and the peculiarities of drawing. The master already had three other students, older boys who had already worked for several years for the masters and were now painting on their own and taking a small fee for it. Oleksa was allowed by the master only occasionally and only for primitive tasks. Instead, Oleksa had to sweep the rooms, heat the stoves, do the housework, and go to town to buy things. For that, he got modest food. But in the evenings, when the master went to a restaurant, and in the mornings, when work in the workshop had not yet begun, Oleksa worked alone with charcoal and paint."</p>
+              <p className="low_margin_top">Here is what Golubovsky writes about Oleksa's first training in A Spread of Mighty Wings. "He was a man of talent, but he had long been prim. He worked according to the patterns he had learned. Nevertheless, Oleksa Novakivsky learned a lot from him from a technical point of view. There he first began to master the technique of painting, and got acquainted with paints, their chemical composition, their proper use, and the peculiarities of drawing. The master already had three other students, older boys who had already worked for several years for the masters and were now painting on their own and taking a small fee for it. Oleksa was allowed by the master only occasionally and only for primitive tasks. Instead, Oleksa had to sweep the rooms, heat the stoves, do the housework, and go to town to buy things. For that, he got modest food. But in the evenings, when the master went to a restaurant, and in the mornings, when work in the workshop had not yet begun, Oleksa worked alone with charcoal and paint."</p>
               <div className="low_margin_top img">
                 <img src={Sticks} alt="" />
                 <p className="caption">Planing sticks</p>
@@ -84,7 +87,7 @@ function MapRoad() {
               <p className="low_margin_top">"Novakivsky did not like painting iconostases. He would rather go to the sea and paint landscapes with the colors that Klymenko scraped off his palette," Sas-Zalozetskyi recalled.When Oleksa realized that there was nothing more to learn from Klymenko, he left his studies and did not accept his teacher's offer to study to become a master.</p>
             </div>
             <div className="right_side">
-              <div className="high_margin_top img">
+              <div className="low_margin_top img">
                 <img src={Pumpkins} alt="" />
                 <p className="caption">Pumpkins. 1892 р. Oil on boardPrivate collection</p>
               </div>
@@ -93,9 +96,46 @@ function MapRoad() {
           </div>
         </div>
 
-        <div className="second_period">
+        <div className="title">
           <div className="main_title show_up element-animation"><h2>Krakow Academy of Fine Arts.</h2></div>
-            
+        </div>
+
+        <div className="second_period">
+          <div className="page">
+            <div className="left_side">
+              <div className="img">
+                <img src={Leon} alt="" />
+                <p className="caption">Leon Vychulkovsky</p>
+              </div>
+              <p className="mid_margin_top">Under Falat, the Krakow School of Drawing became an academy, one of the best art universities in Eastern Europe. Artists who fought against academic conservatism and defended creative positions, such as the aesthetics of Secession, Impressionism, and Symbolism, were invited there. During his studies at the academy and a four-year internship with Leon Vychulkovskyi, Novakivskyi created hundreds of impressionistic landscapes, conveying the impression of instant sensations and experiences through sketchy strokes and the effects of changing light. Novakivsky began to fill the landscapes with deep meaning and allegorical connotations.</p>
+              <p className="low_margin_top">Novakivsky is the most successful graduate of the Krakow Academy of Fine Arts in terms of the number of medals and awards. From the second year of his studies, he was recognized by the Academy, but for unknown reasons, he lost his scholarship from the Brzezowski. His friend Holubowsky in The Spread of Mighty Wings suggests that it was because he signed his paintings in Ukrainian. At that time, he had to sell his paintings and drawings to make a living. He is considered a student of Jan Matejko. Stanisław Wyspianski and Jacek Malczewski, as well as Josef Meghofer, the luminaries of Polish symbolism, had a special influence on the formation of his figurative thinking. </p>
+            </div>
+              <div className="right_side">
+                <p>An artist from Krakow who taught painting to the children of local nobility was impressed by Oleksa's talent. He persuaded the Brzozowski family to give him the scholarship to continue his studies at the Krakow Academy of Fine Arts.</p>
+                <p className="low_margin_top">Upon entering the Academy, it turned out that he did not have a matura (basic education at the time), but his talent impressed the admission committee so much that the rectorate specially organized a separate general education exam to accept Oleksa as a student of the Academy.</p>
+                <div className="img mid_margin_top">
+                  <img src={Yan} alt="" />
+                  <p className="caption">Yan Mateyko</p>
+                </div>
+                <p className="low_margin_top">He was one of the key figures in the transformation of contemporary art and the transition from realism to modernism. Between Impressionism and Post-Impressionism, Novakivsky developed his own style, which currently has no name. Its peculiarity is the pulsation of lines, which reflects the energy tension of the interwar period. This multilayered living contour is reminiscent of the works of Munch and Van Gogh.</p>
+              </div>
+          </div>
+
+          <div className="page">
+            <div className="left_side">
+              <p className="mid_margin_top">In 1900, he completed his studies with a gold medal and a scholarship, and letters of recommendation to continue his studies abroad. But Novakivskyi did not take advantage of that scholarship, because he did not want to change his nationality, which was required for receiving the scholarship.</p>
+              <p className="mid_margin_top">A new student center was established in Krakow, inheriting the traditions of the previous community and was called the Ukrainian Community in Krakow. The Krakow community members created a library, and organized numerous cultural and educational events, including lectures on current socio-political and Ukrainian studies topics, joint excursions, Ukrainian parties, and concerts. </p>
+              <p className="mid_margin_top">In Krakow, Oleksa Novakivsky became friends with Bohdan Lepkyi, who was teaching Ukrainian language and literature at the Jagiellonian University. Lepkyi's apartment gathered the effective leaders of the Ukrainian community of the time and was the center of the Ukrainian community. Vasyl Stefanyk, Osyp Kurylas, Modest Sosenko, Ivan Trush, and Viacheslav Lypynsky came there. </p>
+              <p className="low_margin_top margin_left ">Later, Novakivsky's home in Lviv would become a similar center of the Ukrainian cultural elite.</p>
+            </div>
+            <div className="right_side">
+              <p className="high_margin_top">Members of the Ukrainian community in Krakow cooperated with the Prosvita Society, Ukrainian communities of Naddniprianshchyna, the Russian-Ukrainian Radical Party, and the Revolutionary Ukrainian Party. In 1930, the community numbered about 400 students.</p>
+              <div className="img mid_margin_top">
+                <img src={Fair} alt="" />
+                <p className="caption">A fair in Ukraine. 1903</p>
+              </div>
+            </div>
+          </div>
         </div>
         <div style={{height:2000}}></div>
     </div>
