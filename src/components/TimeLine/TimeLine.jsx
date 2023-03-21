405a2d3b91d748868e3971fb3b0074b82dcdff94
stasik132 <stas1705200563@gmail.com>
2023-03-21 02:21:35 +0200

    add timeline anim

--- src/components/TimeLine/TimeLine.jsx
+++ src/components/TimeLine/TimeLine.jsx
@@ -1,18 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './TimeLine.css'
 function TimeLine() {
+  const [active,setActicve] = useState(false)
+  
+  useEffect(()=>{
+    const timeShow = () =>{
+    if(window.scrollY >= 1000){
+      setActicve(true)
+    }
+    else {
+      setActicve(false)
+    }
+  }
+    window.addEventListener("scroll", timeShow);
+    return () => {
+    window.removeEventListener("scroll", timeShow);
+};
+  },[])
   return (
     <div>
+        <div className={active ? "time_act" : "time_act_hide" }>
 
-        <div className="timeline">
-          <div className="periods">
-            <p className='period period-1'>1872 - 1899</p>
-            <p className='period period-2'>1900-1902</p>
-            <p className='period period-3'>1903-1913</p>
-            <p className='period period-4'>1923 - 1934</p>
-            <p className='period period-5'>1935 - 2016</p>
+        
+          <div className="timeline" >
+            <div className="periods">
+              <div className="hover-underline-animation"><p className='period period-1'>  1872 - 1899</p></div>
+              <div className='hover-underline-animation'> <p className='period period-2'> 1900-1902</p></div>
+              <div className='hover-underline-animation'><p className='period period-3'> 1903-1913</p></div>
+              <div className='hover-underline-animation'><p className='period period-4'>1923 - 1934</p></div>
+              <div className='hover-underline-animation'><p className='period period-5'>1935 - 2016</p></div>
+            </div>
           </div>
         </div>
+        <div style={{height:2000}}></div>
 
     </div>
   )
