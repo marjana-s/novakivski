e7f8fd793e19dad4bbb7b27cdaad712f155c5682
Chopyk Marjana <77576659+marjana-s@users.noreply.github.com>
2023-04-17 10:22:51 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React, {} from 'react';
-
+import React from 'react';
+import logo from './logo.svg';
+import Header from './components/header'
+import Home from './components/home';
+import Artgallery from './components/ArtGallery'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Footer from './components/footer';
 
 function App() {
-
-  
-
   return (
-    <div >
-
-      hello world
-      
-    </div>
+    <div className="App">
+      <Router>
+      <Header></Header>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/artgaery" element={<Artgallery/>} />
+      </Routes>
+        <Footer></Footer>
+      </Router>
+ </div>
   );
 }
 
---
+++ src/components/CarouselBox.js
@@ -0,0 +1,211 @@
+import React, { Component } from "react";
+import Carousel from "react-bootstrap/Carousel";
+import BGART from "../components/img/BGART.png";
+import BGART1 from "../components/img/BGART1.png";
+import BGART2 from "../components/img/BGART2.png";
+import BGART3 from "../components/img/BGART3.png";
+import BGART4 from "../components/img/BGART4.png";
+import BGART5 from "../components/img/BGART5.png";
+import BGART6 from "../components/img/BGART6.png";
+import BGART7 from "../components/img/BGART7.png";
+
+export default class CarouselBox extends Component {
+  render() {
+    return (
+      <Carousel>
+        <Carousel.Item>
+          <img className="d-block w-100" src={BGART} alt="main" />
+          <Carousel.Caption>
+            <div class='CarouselBody'>
+            <div class="CarouselMenu">
+              <h1 class="CarouselTitle">Арт-галерея Новаківського</h1>
+              <p class="CarouselText">
+                Талановитим і темпераментним експонентом краківського
+                постімпресіонізму був уродженець зазбручанського Поділля Олекса
+                Новаківський. Учень Яна Матейка, Унежицького й Леона
+                Вичулковського, він дуже багато завдячував Мальчевському.
+              </p>
+              <div>
+                <button class="CarouselButton">Донат</button>
+                <button class="CarouselButton">Арт галерея</button>
+              </div>
+            </div>
+            <div class='CarouselAbout'>
+              <div class="line"></div>
+              <p class='CarouselChange'>Новаківський Олекса (1872-1935). Автопортрет із пензлем.</p>
+            </div>
+            </div>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={BGART1} alt="main" />
+          <Carousel.Caption>
+            <div class='CarouselBody'>
+            <div class="CarouselMenu">
+              <h1 class="CarouselTitle">Арт-галерея Новаківського</h1>
+              <p class="CarouselText">
+                Талановитим і темпераментним експонентом краківського
+                постімпресіонізму був уродженець зазбручанського Поділля Олекса
+                Новаківський. Учень Яна Матейка, Унежицького й Леона
+                Вичулковського, він дуже багато завдячував Мальчевському.
+              </p>
+              <div>
+                <button class="CarouselButton">Донат</button>
+                <button class="CarouselButton">Арт галерея</button>
+              </div>
+            </div>
+            <div class='CarouselAbout'>
+              <div class="line"></div>
+              <p class='CarouselChange'>Новаківський Олекса (1872-1935). Автопортрет із пензлем.</p>
+            </div>
+            </div>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={BGART2} alt="main" />
+          <Carousel.Caption>
+            <div class='CarouselBody'>
+            <div class="CarouselMenu">
+              <h1 class="CarouselTitle">Арт-галерея Новаківського</h1>
+              <p class="CarouselText">
+                Талановитим і темпераментним експонентом краківського
+                постімпресіонізму був уродженець зазбручанського Поділля Олекса
+                Новаківський. Учень Яна Матейка, Унежицького й Леона
+                Вичулковського, він дуже багато завдячував Мальчевському.
+              </p>
+              <div>
+                <button class="CarouselButton">Донат</button>
+                <button class="CarouselButton">Арт галерея</button>
+              </div>
+            </div>
+            <div class='CarouselAbout'>
+              <div class="line"></div>
+              <p class='CarouselChange'>Новаківський Олекса (1872-1935). Автопортрет із пензлем.</p>
+            </div>
+            </div>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={BGART3} alt="main" />
+          <Carousel.Caption>
+            <div class='CarouselBody'>
+            <div class="CarouselMenu">
+              <h1 class="CarouselTitle">Арт-галерея Новаківського</h1>
+              <p class="CarouselText">
+                Талановитим і темпераментним експонентом краківського
+                постімпресіонізму був уродженець зазбручанського Поділля Олекса
+                Новаківський. Учень Яна Матейка, Унежицького й Леона
+                Вичулковського, він дуже багато завдячував Мальчевському.
+              </p>
+              <div>
+                <button class="CarouselButton">Донат</button>
+                <button class="CarouselButton">Арт галерея</button>
+              </div>
+            </div>
+            <div class='CarouselAbout'>
+              <div class="line"></div>
+              <p class='CarouselChange'>Новаківський Олекса (1872-1935). Автопортрет із пензлем.</p>
+            </div>
+            </div>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={BGART4} alt="main" />
+          <Carousel.Caption>
+            <div class='CarouselBody'>
+            <div class="CarouselMenu">
+              <h1 class="CarouselTitle">Арт-галерея Новаківського</h1>
+              <p class="CarouselText">
+                Талановитим і темпераментним експонентом краківського
+                постімпресіонізму був уродженець зазбручанського Поділля Олекса
+                Новаківський. Учень Яна Матейка, Унежицького й Леона
+                Вичулковського, він дуже багато завдячував Мальчевському.
+              </p>
+              <div>
+                <button class="CarouselButton">Донат</button>
+                <button class="CarouselButton">Арт галерея</button>
+              </div>
+            </div>
+            <div class='CarouselAbout'>
+              <div class="line"></div>
+              <p class='CarouselChange'>Новаківський Олекса (1872-1935). Автопортрет із пензлем.</p>
+            </div>
+            </div>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={BGART5} alt="main" />
+          <Carousel.Caption>
+            <div class='CarouselBody'>
+            <div class="CarouselMenu">
+              <h1 class="CarouselTitle">Арт-галерея Новаківського</h1>
+              <p class="CarouselText">
+                Талановитим і темпераментним експонентом краківського
+                постімпресіонізму був уродженець зазбручанського Поділля Олекса
+                Новаківський. Учень Яна Матейка, Унежицького й Леона
+                Вичулковського, він дуже багато завдячував Мальчевському.
+              </p>
+              <div>
+                <button class="CarouselButton">Донат</button>
+                <button class="CarouselButton">Арт галерея</button>
+              </div>
+            </div>
+            <div class='CarouselAbout'>
+              <div class="line"></div>
+              <p class='CarouselChange'>Новаківський Олекса (1872-1935). Автопортрет із пензлем.</p>
+            </div>
+            </div>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={BGART6} alt="main" />
+          <Carousel.Caption>
+            <div class='CarouselBody'>
+            <div class="CarouselMenu">
+              <h1 class="CarouselTitle">Арт-галерея Новаківського</h1>
+              <p class="CarouselText">
+                Талановитим і темпераментним експонентом краківського
+                постімпресіонізму був уродженець зазбручанського Поділля Олекса
+                Новаківський. Учень Яна Матейка, Унежицького й Леона
+                Вичулковського, він дуже багато завдячував Мальчевському.
+              </p>
+              <div>
+                <button class="CarouselButton">Донат</button>
+                <button class="CarouselButton">Арт галерея</button>
+              </div>
+            </div>
+            <div class='CarouselAbout'>
+              <div class="line"></div>
+              <p class='CarouselChange'>Новаківський Олекса (1872-1935). Автопортрет із пензлем.</p>
+            </div>
+            </div>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={BGART7} alt="main" />
+          <Carousel.Caption>
+            <div class='CarouselBody'>
+            <div class="CarouselMenu">
+              <h1 class="CarouselTitle">Арт-галерея Новаківського</h1>
+              <p class="CarouselText">
+                Талановитим і темпераментним експонентом краківського
+                постімпресіонізму був уродженець зазбручанського Поділля Олекса
+                Новаківський. Учень Яна Матейка, Унежицького й Леона
+                Вичулковського, він дуже багато завдячував Мальчевському.
+              </p>
+              <div>
+                <button class="CarouselButton">Донат</button>
+                <button class="CarouselButton">Арт галерея</button>
+              </div>
+            </div>
+            <div class='CarouselAbout'>
+              <div class="line"></div>
+              <p class='CarouselChange'>Новаківський Олекса (1872-1935). Автопортрет із пензлем.</p>
+            </div>
+            </div>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    );
+  }
+}
